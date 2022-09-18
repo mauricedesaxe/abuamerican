4026e081ee3287e3de866551979c1d2f8c2b38ac
Alex Lazar <alex2012ro@gmail.com>
2022-09-18 18:37:12 +0300

    feat(fe): newsletter form API integration

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
 
-export const FormBtn = ({ cta }: { cta: string }) => {
+export const FormBtn = ({
+  cta,
+  isDisabled = false,
+}: {
+  cta: string;
+  isDisabled?: boolean;
+}) => {
   return (
     <button
+      disabled={isDisabled}
       type="submit"
-      className="inline-block bg-blue-500 border border-transparent rounded-md py-2 px-4 text-base font-medium text-gray-50 transition-colors hover:opacity-90"
+      className="inline-block bg-blue-500 border border-transparent rounded-md py-2 px-4 text-base font-medium text-gray-50 transition-colors hover:opacity-90
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed"
     >
       {cta}
     </button>
--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,12 +1,42 @@
 import { FormEvent, useState } from "react";
 import { FormBtn } from "./Buttons";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 export const NewsletterForm = () => {
   const [email, setEmail] = useState("");
+  const [description, setDescription] = useState(
+    "Signup to my newsletter for regular updates and exclusive content."
+  );
+  const [cta, setCta] = useState("Signup");
+  const [displayInputs, setDisplayInputs] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const mutation = useMutation(
+    () => axios.get(`/api/saveEmail?email=${email}`),
+    {
+      onMutate: () => {
+        setDescription("We are saving your email...");
+        setCta("Loading...");
+        setIsDisabled(true);
+      },
+      onSuccess: () => {
+        setDescription("Thanks for signing up!");
+        setCta("Done");
+        setDisplayInputs(false);
+        setIsDisabled(true);
+      },
+      onError: () => {
+        setDescription("Something went wrong. Please try again.");
+        setCta("Try again");
+        setIsDisabled(false);
+      },
+    }
+  );
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("email", email);
+    mutation.mutate();
   };
 
   return (
@@ -14,30 +44,32 @@ export const NewsletterForm = () => {
       <form onSubmit={(e) => handleSubmit(e)} method="POST">
         <div className="overflow-hidden shadow sm:rounded-md">
           <div className="bg-white px-4 py-5 sm:p-6">
-            <p className="mt-3 text-xl text-black mb-4">
-              Signup to my newsletter for regular updates and exclusive content.
-            </p>
-            <div className="grid grid-cols-6 gap-6">
-              <div className="col-span-6">
-                <label
-                  htmlFor="email-address"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email address
-                </label>
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  autoComplete="email"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-                />
+            <p className="mt-3 text-xl text-black mb-4">{description}</p>
+            {displayInputs ? (
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    autoComplete="email"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                  />
+                </div>
               </div>
-            </div>
+            ) : (
+              ""
+            )}
           </div>
           <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-            <FormBtn cta="Sign up" />
+            <FormBtn cta={cta} isDisabled={isDisabled} />
           </div>
         </div>
       </form>
